eca8bc6ebe857ab3c1f243e31da221558d53cf9b
Pranjal Singh <128744382+spranjal3301@users.noreply.github.com>
2023-09-19 11:27:31 +0530

    Time: 10 ms (95.85%), Space: 19.4 MB (36.14%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -10,30 +10,31 @@
  * };
  */
 class Solution {
-private:
-    int mod=1e9+7 ;
-    void solve(TreeNode* root, int targetSum,vector<int> path,int &count){
-        if(!root)return;
-
-        path.push_back(root->val);
-        solve(root->left,targetSum,path,count);
-        solve(root->right,targetSum,path,count);
-        
-        int sum=0;
-        for(int i=path.size()-1;i>=0;i--){
-            sum+=path[i];
-            if(sum==targetSum)count++;
-            sum=sum%mod;
-        }
-        path.pop_back();
-    }
+    int mod=1e9+7;
 public:
-    int pathSum(TreeNode* root, int targetSum) {
-        vector<int> path;
-        int count=0;
-        solve(root,targetSum,path,count);
-        return count;
-        
-        
+  void solve(TreeNode* root, int targetSum, vector<int>& path, int& count, unordered_map<int, int>& prefixSumCount, int currentSum = 0) {
+    if (!root) return;
+    currentSum += root->val;
+    currentSum=currentSum%mod;
+    // Check if there is a valid path ending at this node
+    if (prefixSumCount.find(currentSum - targetSum) != prefixSumCount.end()) {
+        count += prefixSumCount[currentSum - targetSum];
     }
+    // Update the prefix sum count map
+    prefixSumCount[currentSum]++;
+    // Recurse on left and right subtrees
+    solve(root->left, targetSum, path, count, prefixSumCount, currentSum);
+    solve(root->right, targetSum, path, count, prefixSumCount, currentSum);
+    // Restore the prefix sum count map for backtracking
+    prefixSumCount[currentSum]--;
+}
+
+int pathSum(TreeNode* root, int targetSum) {
+    vector<int> path;
+    int count = 0;
+    unordered_map<int, int> prefixSumCount;
+    prefixSumCount[0] = 1; // Initialize with a prefix sum of 0
+    solve(root, targetSum, path, count, prefixSumCount);
+    return count;
+}
 };
