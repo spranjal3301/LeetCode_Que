d81d951155a4fd72c278e88a8a907ea18b5addff
Pranjal Singh <128744382+spranjal3301@users.noreply.github.com>
2023-10-15 00:38:39 +0530

    Time: 270 ms (77.48%), Space: 117.4 MB (23.37%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -0,0 +1,66 @@
+class MedianFinder {
+    priority_queue<int,vector<int>,greater<int>> mipq;
+    priority_queue<int> mxpq;
+    double median;
+    //! maxHeap a | Median | min Heap b
+public:
+    MedianFinder() {median=INT_MIN;} 
+
+    void addNum(int num) {
+        switch (signum(mxpq.size(),mipq.size()))
+        {
+
+        case 0:
+            if(num>median){
+                mipq.push(num);
+                median=mipq.top();
+            }
+            else{
+              mxpq.push(num);
+              median=mxpq.top();  
+            } 
+            break;
+        case 1:
+            if(num>median){
+                mipq.push(num);
+                median=(mxpq.top()+mipq.top())/2.0; 
+            }
+            else{
+              mipq.push(mxpq.top());
+              mxpq.pop();  
+              mxpq.push(num);
+              median=(mxpq.top()+mipq.top())/2.0; 
+            } 
+            break;
+        case -1:
+            if(num>median){
+                mxpq.push(mipq.top());
+                mipq.pop();
+                mipq.push(num);
+                median=(mxpq.top()+mipq.top())/2.0; 
+            }
+            else{
+              mxpq.push(num);
+              median=(mxpq.top()+mipq.top())/2.0; 
+            }
+            break;
+        }
+
+    }
+    
+    double findMedian() {
+        return median;
+    }
+    int signum(int a,int b){
+        if(a==b)return 0;
+        else if(a>b) return 1;
+        return -1;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
