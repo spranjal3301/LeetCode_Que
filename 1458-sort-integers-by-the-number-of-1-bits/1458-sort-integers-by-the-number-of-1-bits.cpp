b46bd33993b4d36917400d047555ada3c97cd309
Pranjal Singh <128744382+spranjal3301@users.noreply.github.com>
2023-10-30 23:53:16 +0530

    Time: 3 ms (96.58%), Space: 10.4 MB (66.29%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-         vector<multiset<int>> mms(32);
-
-        for(int i=0;i<arr.size();i++){
-            int count=0;
-            for(int b=0;b<32;b++)if((arr[i]>>b)&1)count++;
-
-            mms[count].insert(arr[i]);
-        }
-        int i=0;
-        for(auto &m:mms){
-            for(auto &ms:m){
-                arr[i]=ms;
-                i++;
+        // Custom comparator function for sorting based on count of set bits
+        auto bitCount = [](int num) {
+            int count = 0;
+            while (num) {
+                count += num & 1;
+                num >>= 1;
             }
-        }
-        return arr;
+            return count;
+        };
 
+        // Sort the input array based on bit count using the custom comparator
+        sort(arr.begin(), arr.end(), [&](int a, int b) {
+            int countA = bitCount(a);
+            int countB = bitCount(b);
+
+            // If the counts are equal, sort by the number itself
+            if (countA == countB) {
+                return a < b;
+            }
+
+            return countA < countB;
+        });
+
+        return arr;
     }
 };
